1ea28409c978a3014485a65ca059040add9c3071
Alex Bilger <alexbilger0@gmail.com>
2025-03-17 16:42:34 +0100

    integrate ImNodeFlow

--- SofaImGui/src/SofaImGui/windows/NodeGraph.cpp
+++ SofaImGui/src/SofaImGui/windows/NodeGraph.cpp
@@ -21,15 +21,69 @@
  ******************************************************************************/
 #include <SofaImGui/windows/NodeGraph.h>
 #include <imgui.h>
-#include <imgui_node_editor.h>
+#include <ImNodeFlow.h>
 
 namespace windows {
+
+struct NodeNode : ImFlow::BaseNode
+{
+    explicit NodeNode(sofa::simulation::Node::SPtr node)
+    {
+        this->setTitle(node->getName());
+    }
+};
+
+struct ComponentNode : ImFlow::BaseNode
+{
+    explicit ComponentNode(sofa::core::objectmodel::BaseObject::SPtr component)
+    {
+        this->setTitle(component->getName());
+    }
+};
+
+void addNode(ImFlow::ImNodeFlow& editor, sofa::simulation::Node::SPtr node, std::size_t depth)
+{
+    editor.addNode<NodeNode>(ImVec2{depth * 150.f, 0.f}, node);
+
+    ++depth;
+    for (auto& child : node->getChildren())
+    {
+        if (auto* childNode = dynamic_cast<sofa::simulation::Node*>(child))
+        {
+            addNode(editor, childNode, depth);
+        }
+    }
+
+    float height = 0;
+    for (auto& child : node->getNodeObjects())
+    {
+        editor.addNode<ComponentNode>(ImVec2{depth * 150.f, height += 50.f}, child);
+    }
+}
+
+void addRootNode(ImFlow::ImNodeFlow& editor, sofa::simulation::Node::SPtr node)
+{
+    std::size_t depth = 0;
+    addNode(editor, node, depth);
+}
+
 void showNodeGraph(sofa::core::sptr<sofa::simulation::Node> groot, const char* const& windowNameNodeGraph, WindowState& winManagerNodeGraph)
 {
     if (*winManagerNodeGraph.getStatePtr())
     {
         if (ImGui::Begin(windowNameNodeGraph, winManagerNodeGraph.getStatePtr()))
         {
+            static std::map<sofa::simulation::Node*, ImFlow::ImNodeFlow> nodeEditor;
+            auto nodeIt = nodeEditor.find(groot.get());
+            if (nodeIt == nodeEditor.end())
+            {
+                const auto res = nodeEditor.insert(std::make_pair(groot.get(), ImFlow::ImNodeFlow()));
+                if (res.second) {
+                    nodeIt = res.first;
+                }
+                addRootNode(nodeIt->second, groot);
+            }
+            nodeIt->second.update();
         }
         ImGui::End();
 
